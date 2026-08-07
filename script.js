30e0cb2d3038913daa8f97c6ab07dd3fe52ba1f3
FredON002 <fredrickomondi.n99@gmail.com>
2026-08-07 17:40:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const n=document.querySelector('.nav'),b=document.querySelector('#menu');b.onclick=()=>n.classList.toggle('open');document.querySelectorAll('nav a').forEach(a=>a.onclick=()=>n.classList.remove('open'));
